ee9f50cf0560a0821f69bfd20729dbe1c1297447
aured_0x2747ff <ajcd+prj@opmbx.org>
2016-05-25 21:58:29 +0200

    Added The Descent (unsolved)

---
+++ single_player/easy/the_descent.py
@@ -0,0 +1,29 @@
+"""The Descent
+
+Description:
+
+The enterprise is in danger: drawn towards the surface of an unknown
+planet, it is at risk of crashing against towering mountains.
+
+Help Kirk and Spock destroy the mountains... Save the enterprise!
+
+Topic: search in an array
+"""
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while True:
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain, from 9 to 0.
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+    # The number of the mountain to fire on.
+    print("4")
