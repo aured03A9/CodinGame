6ccba6bb5201cd93b6f0977242906e204096c307
aured_0x2747ff <aured+prj@openmailbox.org>
2016-11-23 21:32:21 +0100

    Updated Mars Lander - Episode 1

--- training/easy/mars-lander-episode-1.py
+++ training/easy/mars-lander-episode-1.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""Mars Lander - Episode 1
-
-The Goal:
-
-The goal for your program is to safely land the "Mars Lander" shuttle,
-the landing ship which contains the Opportunity rover.
-Mars Lander is guided by a program, and right now the failure rate for landing
-on the NASA simulator is unacceptable.
-
-Note that it may look like a difficult problem, but in reality the problem is
-easy to solve.
-This puzzle is the first level among three, therefore, we need to present you
-some controls you won't need in order to complete this first level.
-"""
-
-import sys
-import math
-
 # the number of points used to draw the surface of Mars.
 surface_n = int(input())
 for i in range(surface_n):
