c4aa89346609d1066795f86d4bf6fd974cf01470
aured_0x2747ff <aured+prj@openmailbox.org>
2016-11-23 21:33:54 +0100

    Updated Temperatures

--- training/easy/temperatures.py
+++ training/easy/temperatures.py
@@ -1,32 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""Temperatures
-
-The Goal:
-
-You have to analyze records of temperature to find the closest to zero.
-"""
-
-import sys
-import math
-
 # the number of temperatures to analyse
 n = int(input())
 # the n temperatures expressed as integers ranging from -273 to 5526
-temps = input().split(' ')
+temps = [int(i) if i else 0 for i in input().split(' ')]
 
-if n == 0:
-    print(0)
-else:
-    p = 0
-    for i in range(n):
-        temps[i] = int(temps[i])
-        if p == 0:
-            p = temps[i]
-        elif abs(p) == abs(temps[i]):
-            if p < temps[i]:
-                p = temps[i]
-        elif abs(p) > abs(temps[i]):
-            p = temps[i]
-    print(p)
+print(sorted(temps, key=lambda x: (abs(x), -x))[0])
+#print(min(temps, default=0, key=lambda x: abs(x)))
