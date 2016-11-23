526c10203320602badd5200b6df20d68347476dd
aured_0x2747ff <aured+prj@openmailbox.org>
2016-11-23 21:29:16 +0100

    Updated The Descent

--- training/easy/the-descent.py
+++ training/easy/the-descent.py
@@ -1,26 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""The Descent
-
-The Goal:
-
-Destroy the mountains before your starship collides with one of them.
-For that, shoot the highest mountain on your path.
-"""
-
-import sys
-import math
-
-mountain = {}
-
+# game loop
 while True:
-    for i in range(8):
-        # represents the height of one mountain, from 9 to 0.
-        mountain_h = int(input())
-        mountain[i] = mountain_h
-
-    sorted_mountain = sorted(mountain, key=mountain.get, reverse=True)
-
-    # The index of the mountain to fire on.
-    print(sorted_mountain[0])
+    mountains = [input() for i in range(8)]
+    print(mountains.index(max(mountains)))
